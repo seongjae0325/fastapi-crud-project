a4cc613d12a5325a002d8694e8788a200b8c7bbd
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2022-10-07 23:55:15 +0900

    Feat get cities list api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import requests
+
 app = FastAPI()
 
 db = []
@@ -14,3 +16,14 @@ def create_city(city : City):
     db.append(city.dict())
 
     return db[-1]
+
+@app.get("/cities")
+def get_cities():
+    print(db)
+    results = []
+    for city in db:
+        strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
+        r = requests.get(strs) 
+        cur_time = r.json()["datetime"]
+        results.append({"name": city["name"], "timezone": city["timezone"], "current_time": cur_time})
+    return results
